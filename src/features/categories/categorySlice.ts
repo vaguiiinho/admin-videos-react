d2525980ee2a1bd7783d595a057caa03be1f8029
vaguiiinho <avanlendario@gmail.com>
2024-11-23 18:21:32 -0300

    Criando Slice inicial de categoria

---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Category {
+
+    id: string;
+    name: string;
+    description: string | null;
+    isActive: boolean;
+    deleted_at: null | string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+const category: Category = {
+    id: "0ce68ddd-4981-4ee2-a23b-a01452b96b01",
+    name: "Technology",
+    description: "Latest technological news and trends",
+    isActive: true,
+    deleted_at: null,
+    createdAt: "2022-05-12T14:30:00Z",
+    updatedAt: "2022-05-12T14:30:00Z"
+}
+
+const categories = [
+    category,
+    { ...category, id: "1ce68ddd-4981-4ee2-a23b-a01bbc8c8bb", name: "Peach" },
+    { ...category, id: "4ce68ddd-4981-4ee2-a23b-a01bbc8c8bc", name: "Orange" },
+    { ...category, id: "5ce68ddd-4981-4ee2-a23b-a01bbc8c8bc", name: "Grape" },
+]
+
+export const initialState = categories
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState,
+    reducers: {
+        createCategory: (state, action) => {},
+        listCategory: (state, action) => {},
+        updateCategory: (state, action) => {},
+    }
+})
+
+export default categoriesSlice.reducer
