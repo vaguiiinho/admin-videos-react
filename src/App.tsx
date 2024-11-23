ce2ad3d8b2276a0149a7eff92c36412889e86b2a
vaguiiinho <avanlendario@gmail.com>
2024-11-23 16:58:26 -0300

    criando tema e adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
-import { Box, createTheme, ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider, Typography } from '@mui/material';
 import { Header } from './components/Header';
 import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+import { Route, Routes } from 'react-router-dom';
 
-const theme = createTheme({})
+
+export const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h1" component="h1">
+        Home Page
+      </Typography>
+    </Box>
+  )
+}
+
+export const About = () => {
+  return (
+    <Box>
+      <Typography variant="h1" component="h1">
+        About Page
+      </Typography>
+    </Box>
+  )
+}
+
+
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={appTheme}>
       <Box
         component="main"
         sx={{
           height: '100vh',
-
+          backgroundColor: (theme) => theme.palette.grey[900]
         }}
       >
         <Header />
         <Layout>
-          <h1>Ola</h1>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+
         </Layout>
       </Box>
     </ThemeProvider>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { Box, Container } from "@mui/material";
 export function Layout({ children }: { children: ReactNode }) {
     return (
         <Box>
-            <Container maxWidth="lg" sx={{ my: 4, bg: "white", backgroundColor: '#000' }}>
+            <Container maxWidth="lg" sx={{ my: 4, color: "white"}}>
                 {children}
             </Container>
         </Box>
