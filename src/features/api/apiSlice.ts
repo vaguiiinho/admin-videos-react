af3c822f62ee3cf48ac7eb3b2a37969361288e0c
vaguiiinho <avanlendario@gmail.com>
2024-12-27 21:48:18 -0300

    Interceptando request com msw

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl = 'http://localhost:8000/api'
+export const baseUrl = 'http://localhost:8000/api'
 
 export const apiSlice = createApi({
     reducerPath: 'apiSlice',
--- src/features/categories/CategoryList.test.tsx
+++ src/features/categories/CategoryList.test.tsx
@@ -1,11 +1,29 @@
-
-import { renderWithProviders } from "../../utils/test-utils";
+import { rest } from 'msw'
+import { setupServer } from "msw/node";
+import { renderWithProviders, screen } from "../../utils/test-utils";
 import { CategoryList } from "./CategoryList";
+import { baseUrl } from "../api/apiSlice";
+import { categoryResponse } from "../mocks";
 
+export const handlers = [
+    rest.get(`${baseUrl}/categories`, (req, res, ctx) => {
+        return res(ctx.json(categoryResponse), ctx.delay(150));
+    }),
+]
+export const server = setupServer(...handlers)
 
 describe("CategoryList", () => {
+    afterAll(() => server.close());
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
     it("should render correctly", () => {
         const { asFragment } = renderWithProviders(<CategoryList />);
         expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("should render loading state", () => {
+        renderWithProviders(<CategoryList />);
+        const loading = screen.getByRole("progressbar");
+        expect(loading).toBeInTheDocument();
     });
 });
